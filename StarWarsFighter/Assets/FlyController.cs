ad8db82d72598ae8516f812a130adf92244a7af6
DESKTOP-E8OEJA7\schnu <schnubbi2704@web.de>
2018-04-22 23:48:32 +0200

    add Flycontroller Script

---
+++ StarWarsFighter/Assets/FlyController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyController : MonoBehaviour {
+
+    float speed = 0;
+    float boost = 0.1f;
+    float speedMax = 10;
+    float speedMin = -1;
+    public float RotationSpeed = 2;
+
+
+
+    public float cameraSensitivity = 90;
+    public float climbSpeed = 4;
+    public float normalMoveSpeed = 10;
+    public float slowMoveFactor = 0.25f;
+    public float fastMoveFactor = 3;
+
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+        if (Input.GetKey(KeyCode.W))
+        {
+            if(speed < speedMax)
+            {
+                speed += boost;
+            }
+            else if (speed > speedMax)
+            {
+                speed = speedMax;
+            }
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (speed > speedMin)
+            {
+                speed -= boost;
+            }
+            else if (speed < speedMin)
+            {
+                speed = speedMin;
+            }
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, speed);
+
+        }
+        */
+        Debug.Log(speed);
+
+        rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+        rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        }
+
+
+        if (Input.GetKey(KeyCode.Q)) { transform.position += transform.up * climbSpeed * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.E)) { transform.position -= transform.up * climbSpeed * Time.deltaTime; }
+
+        if (Input.GetKeyDown(KeyCode.End))
+        {
+            Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
+        }
+        //transform.Rotate((Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime), (Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime), 0, Space.World);
+    }
+}
